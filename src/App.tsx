8ba714d8bc3f43c64138c5c16486c4cdd679f2b3
Холина Дарья Анатольевна <holina.darya@otr.ru>
2024-11-18 16:36:49 +0300

    Попытка пофиксить высоту приложения

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useLocation, useNavigate } from 'react-router';
-import { App as AppProvider, Button, ConfigProvider, Layout } from 'antd';
+import { App as AppProvider, ConfigProvider, Layout } from 'antd';
 import ruRU from 'antd/locale/ru_RU';
 
 import './backend/axios';
@@ -10,12 +10,13 @@ import { Footer } from 'layout/footer';
 import { AppRoutes } from 'navigation/routes';
 import { logout } from 'store/auth/reducer';
 import { getUserData } from 'views/auth/login/helpers';
-import { LogoutIcon } from './icons/logout';
 import { getAuth } from './store/auth/selectors';
 import { AppTheme } from './theme';
 import './App.scss';
 
 function App() {
+    // @ts-ignore
+    let resizeInterval = null;
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const { pathname } = useLocation();
@@ -25,12 +26,35 @@ function App() {
         isCheckingToken
     } = useSelector(getAuth);
 
+    const [appHeight, setAppHeight] = useState(0);
+
+    const updateAppHeight = () => {
+        if (window.innerHeight !== appHeight) {
+            setAppHeight(window.innerHeight);
+        }
+    };
+
     useEffect(() => {
         if (isUserLoggedIn) {
             getUserData(user, navigate, pathname);
         } else {
             dispatch(logout());
         }
+
+        resizeInterval = setInterval(() => {
+            window.scrollTo({
+                top: 0
+            });
+            updateAppHeight();
+        }, 500);
+
+        window.addEventListener('resize', updateAppHeight);
+
+        return () => {
+            // @ts-ignore
+            clearInterval(resizeInterval);
+            window.removeEventListener('resize', updateAppHeight);
+        };
     }, []);
 
     return (
@@ -39,7 +63,15 @@ function App() {
             <AppProvider>
                 <NotificationsProvider />
                 <Layout>
-                    <Layout.Content><AppRoutes /></Layout.Content>
+                    <Layout.Content>
+                        <div style={{ color: 'red', padding: 10 }}>
+height:
+{appHeight}
+{' '}
+px
+                        </div>
+                        <AppRoutes />
+                    </Layout.Content>
                     {isUserLoggedIn && !isCheckingToken && <Footer />}
                 </Layout>
             </AppProvider>
